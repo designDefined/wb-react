3434d28e403059ed63863cee1b359b42f07b94f6
designdefined.by@gmail.com <designdefined.by@gmail.com>
2021-10-18 17:24:42 +0900

    Apply mobile sizing again

--- src/components/page/ActText/Introduction/Introduction.js
+++ src/components/page/ActText/Introduction/Introduction.js
@@ -3,9 +3,11 @@ import "./Introduction.scss";
 const Introduction = () => {
   return (
     <div className="Introduction">
-      <p> 쓰고 읽는 것이 더 이상 존중받지 못하는</p>
-      <p> 오늘날의 디지털 매체 환경 속에서</p>
-      <p> 진정한 의미의 글이 자립할 수 있는 새로운 가능성을 모색하다</p>
+      <p>쓰고 읽는 것이</p>
+      <p>더 이상 존중받지 못하는</p>
+      <p>오늘날의 디지털 매체 환경 속에서</p>
+      <p>진정한 의미의 글이 자립할 수 있는</p>
+      <p>새로운 가능성을 모색하다</p>
     </div>
   );
 };
